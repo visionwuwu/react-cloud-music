9518015c3222ca499c125bf9ba69e29bd8d29d13
Visionwuwu <2021664244@qq.com>
2020-10-07 14:06:15 +0800

    Visionwuwu完成路由的配置和Layout布局搭建

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { Provider } from "react-redux"
+import { GlobalStyle } from "./style"
+import { renderRoutes } from "react-router-config"
+import { IconStyle } from "./assets/iconfont/iconfont"
+import routes from "./routes/index"
+import store from "./store/index"
+import { HashRouter } from "react-router-dom"
 import "./fix.css";
 
 function App() {
   return (
-    <>11</>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        {renderRoutes(routes)}
+      </HashRouter>
+    </Provider>
   );
 }
 
